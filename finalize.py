11204e643e509085405be93f999b6c026800eaa9
Hrishikesh Terdalkar <hrishirt@cse.iitk.ac.in>
2024-02-03 01:12:19 +0530

    refactor: add a generic function to apply multiple transliteration transformations

--- finalize.py
+++ finalize.py
@@ -16,7 +16,7 @@ LaTeX Finalization
 import os
 import re
 import argparse
-from typing import Callable
+from typing import Callable, List
 from indic_transliteration.sanscript import transliterate
 from indic_transliteration import sanscript
 
@@ -29,7 +29,7 @@ def transliterate_between(
     to_scheme: str,
     start_pattern: str,
     end_pattern: str,
-    post_hook: Callable[[str], str] = lambda x: x,
+    post_hook: Callable[[str], str] or None = None,
 ) -> str:
     """Transliterate the text appearing between two patterns
 
@@ -66,6 +66,9 @@ def transliterate_between(
     if from_scheme == to_scheme:
         return text
 
+    if post_hook is None:
+        post_hook = lambda x: x
+
     def transliterate_match(matchobj):
         target = matchobj.group(1)
         replacement = transliterate(target, from_scheme, to_scheme)
@@ -85,13 +88,14 @@ def latex_transliteration(
     from_scheme: str,
     to_scheme: str
 ) -> str:
-    """Transliaterate parts of the LaTeX input enclosed in scheme tags
+    """Transliterate parts of the LaTeX input enclosed in scheme tags
 
     A scheme tag is of the form `\\to_scheme_lowercase{}` and is used
     when the desired output is in `to_scheme`.
 
     i.e.,
     - Tags for IAST scheme are enclosed in \\iast{} tags
+    - Tags for ITrans scheme are enclosed in \\itrans{} tags
     - Tags for VH scheme are enclosed in \\vh{} tags
     - ...
 
@@ -121,11 +125,50 @@ def latex_transliteration(
     )
 
 ###############################################################################
-# Example: Devanagari to IAST
+# Apply Chain of Transliterations
 
+
+def apply_transliteration_chain(
+    input_text: str,
+    transliteration_chain: List[List]
+):
+    intermediate_text = input_text
+    for transliteration_config in transliteration_chain:
+        intermediate_text = transliterate_between(
+            intermediate_text,
+            **transliteration_config
+        )
+    return intermediate_text
+
+
+###############################################################################
+# Example: Devanagari to IAST Chain
+
+DEVANAGARI_TO_IAST_TRANSFORMATIONS = [
+    {
+        "from_scheme": sanscript.DEVANAGARI,
+        "to_scheme": sanscript.IAST,
+        "start_pattern": "\\iast{",
+        "end_pattern": "}"
+    },
+    {
+        "from_scheme": sanscript.DEVANAGARI,
+        "to_scheme": sanscript.IAST,
+        "start_pattern": "\\Iast{",
+        "end_pattern": "}",
+        "post_hook": lambda x: x.title()
+    },
+    {
+        "from_scheme": sanscript.DEVANAGARI,
+        "to_scheme": sanscript.IAST,
+        "start_pattern": "\\IAST{",
+        "end_pattern": "}",
+        "post_hook": lambda x: x.upper()
+    }
+]
 
 def devanagari_to_iast(input_text: str) -> str:
-    """Transliaterate parts of the input enclosed in
+    """Transliterate parts of the input enclosed in
     \\iast{}, \\Iast{} or \\IAST{} tags from Devanagari to IAST
 
     Text in \\Iast{} tags also undergoes a `.title()` post-hook.
@@ -141,31 +184,89 @@ def devanagari_to_iast(input_text: str) -> str:
     str
         Text after replacement of text within the IAST tags
     """
-    intermediate_text = transliterate_between(
+
+    return apply_transliteration_chain(
         input_text,
-        from_scheme=sanscript.DEVANAGARI,
-        to_scheme=sanscript.IAST,
-        start_pattern="\\iast{",
-        end_pattern="}"
-    )
-    intermediate_text = transliterate_between(
-        intermediate_text,
-        from_scheme=sanscript.DEVANAGARI,
-        to_scheme=sanscript.IAST,
-        start_pattern="\\Iast{",
-        end_pattern="}",
-        post_hook=lambda x: x.title()
-    )
-    final_text = transliterate_between(
-        intermediate_text,
-        from_scheme=sanscript.DEVANAGARI,
-        to_scheme=sanscript.IAST,
-        start_pattern="\\IAST{",
-        end_pattern="}",
-        post_hook=lambda x: x.upper()
+        DEVANAGARI_TO_IAST_TRANSFORMATIONS
     )
 
-    return final_text
+###############################################################################
+# Example: Bengali to ISO/ITRANS/IAST Chain
+
+
+BENGALI_TRANSFORMATIONS = [
+    {
+        "from_scheme": sanscript.BENGALI,
+        "to_scheme": sanscript.ISO,
+        "start_pattern": "\\textiso{",
+        "end_pattern": "}"
+    },
+    {
+        "from_scheme": sanscript.BENGALI,
+        "to_scheme": sanscript.ISO,
+        "start_pattern": "\\Iso{",
+        "end_pattern": "}",
+        "post_hook": lambda x: x.title()
+    },
+    {
+        "from_scheme": sanscript.BENGALI,
+        "to_scheme": sanscript.ISO,
+        "start_pattern": "\\ISO{",
+        "end_pattern": "}",
+        "post_hook": lambda x: x.upper()
+    },
+    {
+        "from_scheme": sanscript.BENGALI,
+        "to_scheme": sanscript.ITRANS,
+        "start_pattern": "\\textitrans{",
+        "end_pattern": "}"
+    },
+    {
+        "from_scheme": sanscript.BENGALI,
+        "to_scheme": sanscript.IAST,
+        "start_pattern": "\\textiast{",
+        "end_pattern": "}"
+    },
+    {
+        "from_scheme": sanscript.BENGALI,
+        "to_scheme": sanscript.IAST,
+        "start_pattern": "\\Iast{",
+        "end_pattern": "}",
+        "post_hook": lambda x: x.title()
+    },
+    {
+        "from_scheme": sanscript.BENGALI,
+        "to_scheme": sanscript.IAST,
+        "start_pattern": "\\IAST{",
+        "end_pattern": "}",
+        "post_hook": lambda x: x.upper()
+    }
+]
+
+def bengali_to_multiple(input_text: str) -> str:
+    """Transliterate parts of the input enclosed in
+    \\textiast{}, \\Iast{} or \\IAST{} tags from Bengali to IAST
+    \\textiso{}, \\Iso{} or \\ISO{} tags from Bengali to ISO
+    \\textitrans{} tags from Bengali to ITRANS
+
+    Text in \\Iast{} and \\Iso{} tags also undergoes a `.title()` post-hook.
+    Text in \\IAST{} tags \\ISO{} also undergoes a `.upper()` post-hook.
+
+    Parameters
+    ----------
+    input_text : str
+        Input text
+
+    Returns
+    -------
+    str
+        Text after replacement of text within the IAST tags
+    """
+
+    return apply_transliteration_chain(
+        input_text,
+        BENGALI_TRANSFORMATIONS
+    )
 
 ###############################################################################
 
